c91709478c14d275564cb8554d2f31b8097645d6
Dan Garfield <dangarfielduk@hotmail.com>
2022-12-12 13:39:48 +0000

    Add battle location names to metadata json

--- metadata/battle/create-battle-skeleton-metadata.js
+++ metadata/battle/create-battle-skeleton-metadata.js
@@ -5,11 +5,12 @@ const config = require('../../config.json')
 const init = async () => {
   const sceneBin = fs.readJsonSync(path.join(config.outputBattleSceneDirectory, 'scene.bin.json'))
   const exeData = fs.readJsonSync(path.join(config.outputExeDirectory, 'ff7.exe.json'))
-  let data = fs.readdirSync(config.inputBattleBattleDirectory).filter(f => f.toLowerCase().endsWith(('aa'))).map(f => {
+  const zangan = Object.fromEntries(fs.readFileSync('./zangan.lst', 'utf-8').split('\n').map(s => s.split('=')))
+  const data = fs.readdirSync(config.inputBattleBattleDirectory).filter(f => f.toLowerCase().endsWith(('aa'))).map(f => {
     const enemyId = (f.toLowerCase().substring(0, 1).charCodeAt(0) - 97) * 26 + (f.toLowerCase().substring(1, 2).charCodeAt(0) - 97) // TODO - Should improve, guess at this point
 
     // TODO - Need to include other sources (playable characters, weapons etc)
-
+    const id = f.toLowerCase()
     let name = 'Unknown'
     for (let i = 0; i < sceneBin.length; i++) {
       const sceneBinEntry = sceneBin[i]
@@ -21,10 +22,13 @@ const init = async () => {
       const battleCharacterModel = exeData.battleCharacterModels[i]
       if (enemyId === battleCharacterModel.enemyId) { name = battleCharacterModel.name }
     }
-    return {id: f.toLowerCase(), enemyId, name}
+    if (id.substring(0, 2) in zangan) {
+      name = zangan[id.substring(0, 2)] + ' (Battle Location)'
+    }
+    return { id, enemyId, name: name.trim() }
   })
   const outputFile = `${config.metadataDirectory}/ff7-battle-database.json`
   console.log('outputFile', outputFile)
-  fs.writeJsonSync(outputFile, data, {spaces: '\t'})
+  fs.writeJsonSync(outputFile, data, { spaces: '\t' })
 }
 init()
